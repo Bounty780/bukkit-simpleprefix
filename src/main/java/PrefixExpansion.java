3104aba55d55ee24122ed9a4c04467d3307193c9
Bounty780 <51961935+Bounty780@users.noreply.github.com>
2021-03-22 13:31:13 +0300

    Update PrefixExpansion.java

--- src/main/java/PrefixExpansion.java
+++ src/main/java/PrefixExpansion.java
@@ -1,7 +1,7 @@
 package com.flabaliki.simpleprefix; 
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import SimplePrefix.SimplePrefix;
+import com.flabaliki.simpleprefix.SimplePrefix;
 
 import java.io.File;
 import java.text.DateFormat;
