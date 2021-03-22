66992239c8dff6fbed4e6b446d0d7e04872970fc
Bounty780 <51961935+Bounty780@users.noreply.github.com>
2021-03-22 14:15:43 +0300

    Update PrefixExpansion.java

--- src/main/java/PrefixExpansion.java
+++ src/main/java/PrefixExpansion.java
@@ -94,12 +94,12 @@ import org.bukkit.scheduler.BukkitScheduler;
   
         // %example_placeholder1%
         if(identifier.equals("prefix")){
-            return config.getPrefix(player);
+            return SimplePrefixMod.prefix;
         }
 
         // %example_placeholder2%
         if(identifier.equals("suffix")){
-            return config.getPrefix(player);
+            return SimplePrefixMod.suffix;
         }
 
         // We return null if an invalid placeholder (f.e. %example_placeholder3%) 
