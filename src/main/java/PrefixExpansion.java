0152aa72a68f465502e5f4b2294119d27d92434a
Bounty780 <51961935+Bounty780@users.noreply.github.com>
2021-03-22 12:35:20 +0300

    Muuch better

--- src/main/java/PrefixExpansion.java
+++ src/main/java/PrefixExpansion.java
@@ -1,6 +1,7 @@
 package com.flabaliki.simpleprefix; 
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.SimplePrefixExpansion;
 
 import java.io.File;
 import java.text.DateFormat;
