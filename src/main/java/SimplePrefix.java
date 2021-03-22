76bf804375979ad43188a5a4e095f2676a64f8e9
Bounty780 <51961935+Bounty780@users.noreply.github.com>
2021-03-22 14:29:06 +0300

    Update SimplePrefix.java

--- src/main/java/SimplePrefix.java
+++ src/main/java/SimplePrefix.java
@@ -26,8 +26,6 @@ import net.milkbowl.vault.chat.Chat;
 
 public class SimplePrefix extends JavaPlugin implements Listener
 {
-  String prefix = config.getPrefix(player);
-  String suffix = config.getSuffix(player);
   static String pluginName;
   static File pluginFolder;
   static String pluginVersion;
@@ -91,6 +89,8 @@ public class SimplePrefix extends JavaPlugin implements Listener
   public void onPlayerChat(AsyncPlayerChatEvent event)
   {
     Player player = event.getPlayer();
+    String prefix = config.getPrefix(player);
+    String suffix = config.getSuffix(player);
     String world = config.getWorld(player);
     String message = event.getMessage().replaceAll("%", "%%");
     if (template == null) template = "<[time] [world] [prefix][name][suffix]> ";
