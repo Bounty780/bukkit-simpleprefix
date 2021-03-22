134d52f7ca0e2c71caa7935dce51707c1ca22546
Bounty780 <51961935+Bounty780@users.noreply.github.com>
2021-03-22 12:34:46 +0300

    Create SimplePrefixExpansion.java

---
+++ src/main/java/SimplePrefixExpansion.java
@@ -0,0 +1,26 @@
+  
+package com.flabaliki.simpleprefix;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+import me.clip.placeholderapi.PlaceholderAPI;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class SimplePrefixExpansion{}
